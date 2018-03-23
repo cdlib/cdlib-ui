06e88559ccba3107a7925643c981ae8c7f050a8a
JoelUCOP <joel.hagedorn@ucop.edu>
2018-03-23 14:23:09 -0700

    add input object placeholder text

--- elements/objects/input.config.js
+++ elements/objects/input.config.js
@@ -1,11 +1,9 @@
 
-const faker = require('faker/locale/en');
-
 module.exports = {
   collated: true,
   default: '1',
   label: 'Form Inputs',
   context: {
-    text: faker.lorem.words()
+    placeholder: 'placeholder text'
   }
 };
