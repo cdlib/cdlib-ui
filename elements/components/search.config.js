84aaab5fd782975f44c0caf671974c390f926ebe
JoelUCOP <joel.hagedorn@ucop.edu>
2018-03-23 15:52:35 -0700

    create initial search component

---
+++ elements/components/search.config.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  collated: true,
+  default: '1',
+  label: 'Search',
+  context: {
+    label1: {
+      text: 'Search the California Digital Library'
+    },
+    input1: {
+      placeholder: ''
+    },
+    button4: {
+      text: 'Search'
+    },
+  }
+};
