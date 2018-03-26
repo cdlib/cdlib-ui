f41cba20033595504ebe8f3ac8691f77059c7260
JoelUCOP <joel.hagedorn@ucop.edu>
2018-03-26 16:34:06 -0700

    create initial localnav html & styles

--- elements/components/localnav.config.js
+++ elements/components/localnav.config.js
@@ -5,6 +5,18 @@ module.exports = {
   collated: true,
   label: 'Local Nav',
   context: {
-    
+    localnav: {
+      headingText: 'Collection Development & Management',
+      linktext1: 'Licensed Resources',
+      linktext2: 'Shared Print',
+      linktext3: 'Mass Digitization',
+      linktext4: 'Where to Find Our Books',
+      linktext5: 'FAQ',
+      linktext6: 'HathiTrust',
+      linktext7: 'Library Reprints',
+      linktext8: 'Story of the Digital Book',
+      linktext9: 'Shared Cataloging',
+      linktext10: 'Staff Contacts'
+    }
   }
 };
