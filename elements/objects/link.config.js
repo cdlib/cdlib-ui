208fabc7a12dff5dd207484e9924e3f476fd9249
JoelUCOP <joel.hagedorn@ucop.edu>
2018-03-27 13:41:16 -0700

    update link objects and styles

--- elements/objects/link.config.js
+++ elements/objects/link.config.js
@@ -3,17 +3,19 @@ const faker = require('faker/locale/en');
 
 module.exports = {
   collated: true,
-  default: '1',
   label: 'Links',
   context: {
     link1: {
       text: 'standard link'
     },
     link2: {
-      text: 'external link'
+      text: 'underlined standard link on focus/hover'
     },
     link3: {
-      text: 'navigation link'
+      text: 'external link'
+    },
+    link4: {
+      text: 'underlined external link on focus/hover'
     }
   }
 };
