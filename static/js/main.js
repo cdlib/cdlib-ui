3a2b90bb25f034d031d1f29a8293ef1ad1e42da7
JoelUCOP <joel.hagedorn@ucop.edu>
2018-03-29 15:50:10 -0700

    add localnav toggle logic

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,13 @@
+// Show/Hide Toggle //
 
+new Vue({
+  el: '#app',
+  data: {
+    isDisplayed: false
+  },
+  methods: {
+    toggle1: function(){
+      this.isDisplayed = !this.isDisplayed
+    }
+  }
+})
