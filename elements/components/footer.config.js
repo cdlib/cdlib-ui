d12b7bcc2a2a290f3c5e466503074ed5ec927e0a
JoelUCOP <joel.hagedorn@ucop.edu>
2018-04-02 15:29:17 -0700

    create footer dynamic data list-items

--- elements/components/footer.config.js
+++ elements/components/footer.config.js
@@ -1,6 +1,48 @@
+const faker = require('faker/locale/en');
+const linkData1 = [];
+const linkData2 = [];
+const linkData3 = [];
+const linkData4 = [];
+
+for (var i = 0; i < 10; i++) {
+  linkData1.push({
+    link2: {
+      text: faker.lorem.words()
+    }
+  });
+}
+
+for (var i = 0; i < 10; i++) {
+  linkData2.push({
+    link2: {
+      text: faker.lorem.words()
+    }
+  });
+}
+
+for (var i = 0; i < 5; i++) {
+  linkData3.push({
+    link2: {
+      text: faker.lorem.words()
+    }
+  });
+}
+
+for (var i = 0; i < 5; i++) {
+  linkData4.push({
+    link2: {
+      text: faker.lorem.words()
+    }
+  });
+}
+
+
 module.exports = {
   label: 'Global Footer',
   context: {
-    
+    listItem1: linkData1,
+    listItem2: linkData2,
+    listItem3: linkData3,
+    listItem4: linkData4
   }
 };
