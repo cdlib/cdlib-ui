2f230e98f227b4bd32ce770b65f1d72f265eb0ce
JoelUCOP <joel.hagedorn@ucop.edu>
2018-04-09 16:04:16 -0700

    create staff table comp

---
+++ elements/components/staff.config.js
@@ -0,0 +1,26 @@
+const faker = require('faker/locale/en');
+const tableData = [];
+
+for (var i = 0; i < 10; i++) {
+  tableData.push({
+    firstname: {
+      text: faker.name.firstName()
+    },
+    lastname: {
+      text: faker.name.lastName()
+    },
+    position: {
+      text: faker.name.jobTitle()
+    },
+    profile: {
+      isActive: faker.random.boolean()
+    }
+  });
+}
+
+module.exports = {
+  label: 'Staff Table',
+  context: {
+    row: tableData
+  }
+};
