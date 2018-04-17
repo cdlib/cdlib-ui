ee5ae6aee3d3f6be4db49b163171c01ca4832112
JoelUCOP <joel.hagedorn@ucop.edu>
2018-04-17 08:23:07 -0700

    add view-match-media script and logic to localnav comp

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-// Show/Hide Toggle //
+// Show/Hide Toggle and Media Queries //
 
 new Vue({
   el: '#app',
@@ -8,6 +8,17 @@ new Vue({
   methods: {
     toggle1: function(){
       this.isDisplayed = !this.isDisplayed
+    },
+    detectMediaQuery: function(alias, matches) {
+      if (matches)
+        this.isDisplayed = false
     }
+  },
+  mq: {
+    screen1: '(min-width: 480px)',
+    screen2: '(min-width: 760px)',
+    screen3: '(min-width: 870px)',
+    screen4: '(min-width: 1280px)',
+    screen5: '(min-width: 1400px)'
   }
 })
