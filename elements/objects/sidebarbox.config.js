461d30417c14dc47d81eb031b2144b264a44707d
JoelUCOP <joel.hagedorn@ucop.edu>
2018-04-17 13:47:43 -0700

    create sidebar box 2

--- elements/objects/sidebarbox.config.js
+++ elements/objects/sidebarbox.config.js
@@ -1,6 +1,18 @@
 const faker = require('faker/locale/en');
+const fakerData = [];
+
+for (var i = 0; i < 5; i++) {
+  fakerData.push({
+    link: {
+      text: faker.lorem.words(),
+      url: faker.internet.url()
+    }
+  });
+}
 
 module.exports = {
+  collated: true,
+  default: '1',
   label: 'Sidebar Box',
   context: {
     heading: {
@@ -9,6 +21,7 @@ module.exports = {
     },
     sidebarbox: {
       text: faker.lorem.paragraph()
-    }
+    },
+    listItem: fakerData
   }
 };
