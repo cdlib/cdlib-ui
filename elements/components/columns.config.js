2bbdb14a387ca8f7766450f0c03a639a7562df69
JoelUCOP <joel.hagedorn@ucop.edu>
2018-04-27 11:52:18 -0700

    add column items 4 & 5, demo styles, and headings

--- elements/components/columns.config.js
+++ elements/components/columns.config.js
@@ -4,12 +4,23 @@ module.exports = {
   label: 'Columns',
   context: {
     column1: {
+      name: 'A: Local Nav',
       text: faker.lorem.paragraph()
     },
     column2: {
+      name: 'B: Main Content',
       text: faker.lorem.paragraphs()
     },
     column3: {
+      name: 'C: Secondary / Sidebar Content',
+      text: faker.lorem.sentence()
+    },
+    column4: {
+      name: 'Section X',
+      text: faker.lorem.paragraph()
+    },
+    column5: {
+      name: 'Section Y',
       text: faker.lorem.paragraph()
     }
   }
