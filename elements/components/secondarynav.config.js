4768cdf87f101c714ddf161dc684f6c0db9a7522
JoelUCOP <joel.hagedorn@ucop.edu>
2018-05-15 15:05:54 -0700

    rename localnav component to secondarynav

--- elements/components/secondarynav.config.js
+++ elements/components/secondarynav.config.js
@@ -2,11 +2,9 @@
 const faker = require('faker/locale/en');
 
 module.exports = {
-  collated: true,
-  default: '1',
-  label: 'Local Nav',
+  label: 'Secondary Nav',
   context: {
-    localnav: {
+    secondarynav: {
       closeButtonText: 'Close',
       openButtonText: 'Open',
       linktext1: 'Licensed Resources',
