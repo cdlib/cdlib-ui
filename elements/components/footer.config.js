67ad0b8246c64c85d13d885a620b297ec667e789
JoelUCOP <joel.hagedorn@ucop.edu>
2018-05-21 11:34:16 -0700

    refactor footer comp using nested lists

--- elements/components/footer.config.js
+++ elements/components/footer.config.js
@@ -1,17 +1,16 @@
 const faker = require('faker/locale/en');
 const linkData1 = [];
 const linkData2 = [];
-const linkData3 = [];
-const linkData4 = [];
+const list1 = ['About CDL', 'Services & Projects', 'Resources for...', 'Committees & Groups', 'News & Groups'];
 
 function capFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-for (var i = 0; i < 3; i++) {
+for (var i = 0; i < 5; i++) {
   linkData1.push({
     link: {
-      text: capFirstLetter(faker.lorem.words())
+      text: list1[i]
     }
   });
 }
@@ -24,30 +23,11 @@ for (var i = 0; i < 5; i++) {
   });
 }
 
-for (var i = 0; i < 3; i++) {
-  linkData3.push({
-    link: {
-      text: capFirstLetter(faker.lorem.words())
-    }
-  });
-}
-
-for (var i = 0; i < 3; i++) {
-  linkData4.push({
-    link: {
-      text: capFirstLetter(faker.lorem.words())
-    }
-  });
-}
-
-
 module.exports = {
   label: 'Global Footer',
   context: {
-    listItem1: linkData1,
-    listItem2: linkData2,
-    listItem3: linkData3,
-    listItem4: linkData4,
+    list1: linkData1,
+    list2: linkData2,
     heading: {
       level: '2',
     },
