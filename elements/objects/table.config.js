8b9721119c3d75b18c83e56303eb90cf1a0f1c65
JoelUCOP <joel.hagedorn@ucop.edu>
2018-05-21 14:55:50 -0700

    create table object; integrate into staff comp

---
+++ elements/objects/table.config.js
@@ -0,0 +1,43 @@
+const faker = require('faker/locale/en');
+const tableHeader = [];
+const tableData = [];
+const tableHeaderLabels = ['Country', 'Latitude', 'Longitude', 'City', 'Product', 'Inventory ID'];
+
+for (var i = 0; i < 6; i++) {
+  tableHeader.push({
+    cell1: {
+      data: tableHeaderLabels[i]
+    }
+  });
+}
+
+for (var i = 0; i < 15; i++) {
+  tableData.push({
+    cell1: {
+      data: faker.address.country()
+    },
+    cell2: {
+      data: faker.address.latitude()
+    },
+    cell3: {
+      data: faker.address.longitude()
+    },
+    cell4: {
+      data: faker.address.city()
+    },
+    cell5: {
+      data: faker.commerce.productName()
+    },
+    cell6: {
+      data: faker.address.zipCode()
+    }
+  });
+}
+
+module.exports = {
+  label: 'Table',
+  context: {
+    row1: tableHeader,
+    row2: tableData
+  }
+};
