22e9062514ff3e45392a2a0667f681633a6bc31f
JoelUCOP <joel.hagedorn@ucop.edu>
2018-05-24 09:28:04 -0700

    remove unused config.js faker data and logic

--- elements/components/header.config.js
+++ elements/components/header.config.js
@@ -1,24 +1,3 @@
-const fakerGlobalNav = [];
-const fakerTopNav = [];
-const navGlobal = ['Staff Directory', 'Contact CDL', 'Report a Problem', 'System Status'];
-const navTop = ['About CDL', 'Services & Projects', 'Resources for...', 'Committees & Groups', 'News & Groups'];
-
-for (var i = 0; i < 4; i++) {
-  fakerGlobalNav.push({
-    link: {
-      text: navGlobal[i]
-    }
-  });
-}
-
-for (var i = 0; i < 5; i++) {
-  fakerTopNav.push({
-    link: {
-      text: navTop[i]
-    }
-  });
-}
-
 module.exports = {
   label: 'Global Header',
   context: {
@@ -31,9 +10,6 @@ module.exports = {
     },
     button4: {
       text: 'Search'
-    },
-    // Global and top navs:
-    list1: fakerGlobalNav,
-    list2: fakerTopNav
+    }
   }
 };
