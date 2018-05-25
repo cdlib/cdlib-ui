0d8055b77ad4a0cd6efb52f714b91bd6f7d99f29
JoelUCOP <joel.hagedorn@ucop.edu>
2018-05-25 11:00:39 -0700

    create sidebar nav object

---
+++ elements/objects/sidebarnav.config.js
@@ -0,0 +1,21 @@
+const faker = require('faker/locale/en');
+const fakerList1 = [];
+
+for (var i = 0; i < 5; i++) {
+  fakerList1.push({
+    link: {
+      text: faker.commerce.productName()
+    }
+  });
+}
+
+module.exports = {
+  label: 'Sidebar Nav',
+  context: {
+    heading: {
+      level: '2',
+      text: faker.commerce.productName()
+    },
+    list1: fakerList1,
+  }
+};
