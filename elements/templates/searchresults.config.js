bdb4f242ad538c9035f9e3061751cd1aaac69a2c
JoelUCOP <joel.hagedorn@ucop.edu>
2018-06-08 10:36:23 -0700

    create searchresults template

---
+++ elements/templates/searchresults.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  label: 'Search Results',
+  context: {
+    searchresults: {
+      sort: '["sort" component to go here]',
+      results: '["results" component to go here]'
+    }
+  }
+};
