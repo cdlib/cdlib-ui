dfdaf22a286378750f01a0771d2e935586216eb2
JoelUCOP <joel.hagedorn@ucop.edu>
2018-06-11 16:24:47 -0700

    update external link style

--- elements/objects/link.config.js
+++ elements/objects/link.config.js
@@ -24,7 +24,7 @@ module.exports = {
       name: '3', // link--3.hbs
       context: {
         link: {
-          text: 'Blue, underlined by default (external link)',
+          text: 'Blue, underlined by default, external link',
           url: 'wordpress/external/link'
         }
       }
@@ -33,7 +33,7 @@ module.exports = {
       name: '4', // link--4.hbs
       context: {
         link: {
-          text: 'Blue, underlined on focus/hover (external link)',
+          text: 'Blue, underlined on focus/hover, external link',
           url: 'wordpress/external/link'
         }
       }
