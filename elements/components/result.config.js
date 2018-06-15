35705b1177933fe195b3c41b5d1b3005db9033ed
JoelUCOP <joel.hagedorn@ucop.edu>
2018-06-15 14:57:07 -0700

    create search results component

---
+++ elements/components/result.config.js
@@ -0,0 +1,28 @@
+const faker = require('faker/locale/en');
+const fakerData = [];
+
+for (var i = 0; i < 8; i++) {
+  fakerData.push({
+    heading: {
+      level: '3'
+    },
+    link1: false,
+    link2: true,
+    link: {
+      text: faker.commerce.productName()
+    },
+    result: {
+      group: faker.helpers.randomize(['News and Media: CDLINFO', 'Information Services', 'User Experience Design Services', 'Discovery ＆ Delivery Program', 'Collections Program']),
+      doctype: faker.helpers.randomize(['pdf', 'doc', 'xls', 'txt', 'zip', 'html']),
+      quote: faker.lorem.words(30),
+      date: faker.date.month() + ' ' + faker.random.number({min: 1, max: 30}) + ', ' + faker.random.number({min: 2000, max: 2018})
+    }
+  });
+}
+
+module.exports = {
+  label: 'Search Results',
+  context: {
+    list: fakerData,
+  }
+};
