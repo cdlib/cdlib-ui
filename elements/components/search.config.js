eb7029f5f737e3f39ce45575dc1cb5619d8622e1
JoelUCOP <joel.hagedorn@ucop.edu>
2018-06-18 10:31:54 -0700

    add search component 'action' data

--- elements/components/search.config.js
+++ elements/components/search.config.js
@@ -4,6 +4,9 @@ module.exports = {
   default: '1',
   label: 'Search',
   context: {
+    search: {
+      action: '/'
+    },
     label1: {
       for: 'c-header-search-field',
       text: 'Search the California Digital Library'
