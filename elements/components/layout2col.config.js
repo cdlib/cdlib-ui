c8b230c7a10c13bcbe004a1d8e52a937cc1f6d8f
JoelUCOP <joel.hagedorn@ucop.edu>
2018-06-25 16:00:35 -0700

    add header, breadcrumb, and footer layout2col regions

--- elements/components/layout2col.config.js
+++ elements/components/layout2col.config.js
@@ -3,13 +3,26 @@ const faker = require('faker/locale/en');
 module.exports = {
   label: 'Layout 2 Column',
   context: {
+    highlightGrid: true,
     gridItem1: {
-      name: 'Sidebar',
+      name: 'Header',
       text: faker.lorem.paragraph()
     },
     gridItem2: {
-      name: 'Main Content',
+      name: 'Breadcrumb',
+      text: faker.lorem.sentence()
+    },
+    gridItem3: {
+      name: 'Sidebar',
       text: faker.lorem.paragraphs()
+    },
+    gridItem4: {
+      name: 'Main Content',
+      text: faker.lorem.paragraph()
+    },
+    gridItem5: {
+      name: 'Footer',
+      text: faker.lorem.paragraph()
     }
   }
 };
