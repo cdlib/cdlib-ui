52aeef3a21bff30833184e73159f568600dfab31
JoelUCOP <joel.hagedorn@ucop.edu>
2018-06-25 14:50:04 -0700

    add header, breadcrumb, and footer layout3col regions

--- elements/components/layout3col.config.js
+++ elements/components/layout3col.config.js
@@ -4,19 +4,31 @@ module.exports = {
   label: 'Layout 3 Column',
   context: {
     gridItem1: {
-      name: 'A: Local Nav',
+      name: 'Header',
       text: faker.lorem.paragraph()
     },
     gridItem2: {
-      name: 'B: Main Content',
-      text: faker.lorem.paragraphs()
+      name: 'Breadcrumb',
+      text: faker.lorem.sentence()
     },
     gridItem3: {
+      name: 'A: Local Nav',
+      text: faker.lorem.paragraphs()
+    },
+    gridItem4: {
+      name: 'B: Main Content',
+      text: faker.lorem.paragraph()
+    },
+    gridItem5: {
       name: 'Section X',
       text: faker.lorem.paragraph()
     },
-    gridItem4: {
+    gridItem6: {
       name: 'Section Y',
+      text: faker.lorem.paragraph()
+    },
+    gridItem7: {
+      name: 'Footer',
       text: faker.lorem.paragraph()
     }
   }
