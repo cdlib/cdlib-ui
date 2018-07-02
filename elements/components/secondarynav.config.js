ddd54bed5114a1c868e483f376a5ef51e97eec55
JoelUCOP <joel.hagedorn@ucop.edu>
2018-07-02 15:03:48 -0700

    add secondarynav top-level item and 'current page' styles

--- elements/components/secondarynav.config.js
+++ elements/components/secondarynav.config.js
@@ -1,8 +1,9 @@
 const faker = require('faker/locale/en');
 const fakerList1 = [];
 const fakerList2 = [];
+const topMenuItem = faker.commerce.productName()
 
-for (var i = 0; i < 5; i++) {
+for (var i = 0; i < 2; i++) {
   fakerList1.push({
     link: {
       text: faker.commerce.productName()
@@ -10,7 +11,7 @@ for (var i = 0; i < 5; i++) {
   });
 }
 
-for (var i = 0; i < 5; i++) {
+for (var i = 0; i < 3; i++) {
   fakerList2.push({
     link: {
       text: faker.commerce.productName()
@@ -26,7 +27,7 @@ module.exports = {
     },
     link1: true,
     link: {
-      text: faker.commerce.productName()
+      text: topMenuItem
     },
     secondarynav: {
       closeButtonText: 'Close',
