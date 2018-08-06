87cf5f4742e6bfcc419c799268b79217f5696692
JoelUCOP <joel.hagedorn@ucop.edu>
2018-08-06 11:21:56 -0700

    create rss feed component

---
+++ elements/3-components/rssfeed.config.js
@@ -0,0 +1,29 @@
+const faker = require('faker/locale/en');
+const fakerData = [];
+
+for (var i = 0; i < 3; i++) {
+  fakerData.push({
+    heading: {
+      level: '2'
+    },
+    link1: true,
+    link: {
+      text: 'CDL Alert: ' + faker.lorem.sentence()
+    },
+    feed: {
+      date: 7 + i + '/' + '/1' + i * 2 + '/2018'
+    }
+  });
+}
+
+module.exports = {
+  label: 'RSS Feed',
+  context: {
+    heading: {
+      level: '2',
+      text: 'RSS Feed Title'
+    },
+    feed: fakerData
+  },
+  notes: "This component uses the HTML generated from the WP Aggregator Widget, including the `.rss-aggregator` class. There are no uniquely-named CSS classes (like `rssfeed`) used for this component."
+};
