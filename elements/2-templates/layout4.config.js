243668dfd5a4d9855bec89744ccf4312156317c8
JoelUCOP <joel.hagedorn@ucop.edu>
2018-08-07 14:52:21 -0700

    create blog 2 column template

---
+++ elements/2-templates/layout4.config.js
@@ -0,0 +1,32 @@
+const faker = require('faker/locale/en');
+
+module.exports = {
+  label: 'Blog 2 Column',
+  context: {
+    highlightGrid: true,
+    gridItemHeader: {
+      name: 'Header',
+      text: faker.lorem.paragraph()
+    },
+    gridItemBreadcrumb: {
+      name: 'Breadcrumb',
+      text: faker.lorem.sentence()
+    },
+    gridItemMain: {
+      name: 'Main Content',
+      text: faker.lorem.paragraphs()
+    },
+    gridItemSidebar1: {
+      name: 'Sidebar 1',
+      text: faker.lorem.paragraph()
+    },
+    gridItemSidebar2: {
+      name: 'Sidebar 2',
+      text: faker.lorem.paragraph()
+    },
+    gridItemFooter: {
+      name: 'Footer',
+      text: faker.lorem.paragraph()
+    }
+  }
+};
