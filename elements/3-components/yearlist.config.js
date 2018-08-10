dcda7a4205cbf084e22bf2f6244f9dd2c14d6392
JoelUCOP <joel.hagedorn@ucop.edu>
2018-08-10 11:37:50 -0700

    create yearlist component

---
+++ elements/3-components/yearlist.config.js
@@ -0,0 +1,17 @@
+const faker = require('faker/locale/en');
+const fakerData = [];
+
+for (var i = 0; i < 22; i++) {
+  fakerData.push({
+    link: {
+      text: 2018 - i
+    }
+  });
+}
+
+module.exports = {
+  label: 'Year Anchor List',
+  context: {
+    list: fakerData
+  }
+};
