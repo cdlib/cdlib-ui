777265943186427f751aba6a3909145054639432
JoelUCOP <joel.hagedorn@ucop.edu>
2018-08-17 15:29:03 -0700

    create checkbox component

---
+++ elements/3-components/checkbox.config.js
@@ -0,0 +1,18 @@
+const faker = require('faker/locale/en');
+const fakerData = [];
+
+for (var i = 0; i < 6; i++) {
+  fakerData.push({
+    checkbox: {
+      label: faker.commerce.productName()
+    }
+  });
+}
+
+module.exports = {
+  label: 'Checkbox',
+  context: {
+    checkbox: fakerData
+  },
+  notes: 'Markup for this component reflects the WordPress Contact Form 7 plugin.'
+};
