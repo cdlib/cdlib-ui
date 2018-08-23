55fdd1160244c5da0b4a9b85d32f19b52e73e1c1
JoelUCOP <joel.hagedorn@ucop.edu>
2018-08-23 16:17:34 -0700

    render blogmetadata within blogresult component

--- elements/3-components/blogresult.config.js
+++ elements/3-components/blogresult.config.js
@@ -4,17 +4,10 @@ const fakerData = [];
 for (var i = 0; i < 8; i++) {
   fakerData.push({
     heading: {
-      level: '2'
-    },
-    link1: true,
-    link2: false,
-    link: {
+      level: '2',
       text: faker.commerce.productName() + faker.helpers.randomize([' for ', ' with ', ' and ', ': ']) + faker.commerce.productName()
     },
     blogresult: {
-      date: faker.date.month() + ' ' + faker.random.number({min: 1, max: 30}) + ', ' + faker.random.number({min: 2000, max: 2018}),
-      categories: 'Collection Development, Newsletter, Shared Print, Staff News',
-      author: faker.name.findName(),
       quote: faker.lorem.sentence(30)
     }
   });
