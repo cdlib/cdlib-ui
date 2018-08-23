3134f74f4caeab10bf3c2f49d42fb29c8f558d69
JoelUCOP <joel.hagedorn@ucop.edu>
2018-08-23 15:51:35 -0700

    create blogmetadata component

---
+++ elements/3-components/blogmetadata.config.js
@@ -0,0 +1,35 @@
+const faker = require('faker/locale/en');
+const categoryData = [];
+const categories = ['Collection Development', 'Newsletter', 'Shared Print', 'Staff News'];
+const tagData = [];
+const tags = ['resource_change', 'arks', 'citation', 'community', 'open source'];
+
+for (var i = 0; i < categories.length; i++) {
+  categoryData.push({
+    link: {
+      text: categories[i]
+    }
+  });
+}
+
+for (var i = 0; i < tags.length; i++) {
+  tagData.push({
+    link: {
+      text: tags[i]
+    }
+  });
+}
+
+module.exports = {
+  label: 'Blog Metadata',
+  context: {
+    link: {
+      text: faker.name.findName()
+    },
+    blogmetadata: {
+      date: faker.date.month() + ' ' + faker.random.number({min: 1, max: 30}) + ', ' + faker.random.number({min: 2000, max: 2018})
+    },
+    categoryList: categoryData,
+    tagList: tagData
+  }
+};
