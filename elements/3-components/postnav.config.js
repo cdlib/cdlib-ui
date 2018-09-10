3573eeda45ba77ced2dd0dda7a74be346a0bb91f
JoelUCOP <joel.hagedorn@ucop.edu>
2018-09-10 16:05:40 -0700

    create postnav component

---
+++ elements/3-components/postnav.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  label: 'Post Navigation',
+  context: {
+    link1: {
+      link: {
+        text: 'Newer posts'
+      }
+    },
+    link2: {
+      link: {
+        text: 'Older posts'
+      }
+    }
+  }
+};
