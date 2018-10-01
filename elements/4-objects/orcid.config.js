15526571f91f5ab02bf5bdcd5c2cceadedcdf2a2
JoelUCOP <joel.hagedorn@ucop.edu>
2018-10-01 13:51:32 -0700

    create orcid id object

---
+++ elements/4-objects/orcid.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  label: 'Orcid ID',
+  context: {
+    orcid: {
+      url: 'http://orcid.org/0000-0003-2326-6672',
+      text: 'http://orcid.org/0000-0003-2326-6672'
+    }
+  }
+};
