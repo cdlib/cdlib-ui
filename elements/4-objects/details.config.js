c4a45aece338caa72e41bc08d9bc8f5bfbbbf536
JoelUCOP <joel.hagedorn@ucop.edu>
2018-10-04 11:08:16 -0700

    add details-element-polyfill script, add child node to details comp; remove margin style

--- elements/4-objects/details.config.js
+++ elements/4-objects/details.config.js
@@ -7,5 +7,6 @@ module.exports = {
       summary: faker.lorem.sentence(),
       text: faker.lorem.sentences(20)
     }
-  }
+  },
+  notes: 'A child element, like `<p>`, must wrap the content within the parent `<details>` element in order for details-element-polyfill.js to work in IE.'
 };
