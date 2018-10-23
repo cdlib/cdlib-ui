42fa7e3b2551aa88a0459197dbddd3ccffca00e3
JoelUCOP <joel.hagedorn@ucop.edu>
2018-10-23 16:27:56 -0700

    create contact component

---
+++ elements/3-components/contact.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  label: 'Contact',
+  collated: true,
+  default: '1',
+  context: {
+    contact: {
+      button: {
+        text: 'UCOP Contact Information'
+      },
+      link: {
+        text: 'UCSD Contact Information'
+      }
+    }
+  },
+  notes: 'Component element styled for either a `<button>` or `<a>`.'
+};
