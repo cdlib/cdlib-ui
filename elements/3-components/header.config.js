f1f2fe83fa739ffd4455d7235355619c28d0e5e1
JoelUCOP <joel.hagedorn@ucop.edu>
2018-12-14 16:33:39 -0800

    include search comp with data as-is in header comp

--- elements/3-components/header.config.js
+++ elements/3-components/header.config.js
@@ -1,15 +1,3 @@
 module.exports = {
-  label: 'Header',
-  context: {
-    // Search component:
-    label1: {
-      text: 'Search the California Digital Library'
-    },
-    input1: {
-      placeholder: ''
-    },
-    button4: {
-      text: 'Search'
-    }
-  }
+  label: 'Header'
 };
