05042c44bdc528a5ba5fd2538f4b21ea2c2215fc
JoelCDL <joel.hagedorn@ucop.edu>
2019-01-30 18:20:55 -0800

    center pagination, remove arrow links, update aria labels, create pagination range logic

--- elements/3-components/pagination.config.js
+++ elements/3-components/pagination.config.js
@@ -1,11 +1,23 @@
 const faker = require('faker/locale/en');
 const fakerData = [];
+const minNum = 1;
+const startNum = 11;
+const endNum = 16;
+const maxNum = 47;
 
-for (var i = 0; i < 5; i++) {
+// range function below inspired from: https://stackoverflow.com/questions/8273047/javascript-function-similar-to-python-range
+
+function* range(start, stop) {
+  for (let i = start; 1 > 0 ? i < stop : i > stop; i++) {
+    yield i;
+  }
+}
+
+for (let i of range(startNum, endNum, 1)) {
   fakerData.push({
     pagination: {
-      number: i + 1,
-      isSelected: i == 2 // #3 gets selected
+      number: i,
+      isSelected: i == Math.floor((startNum + endNum) / 2) // nearest middle number within range gets selected
     }
   });
 }
@@ -13,6 +25,8 @@ for (var i = 0; i < 5; i++) {
 module.exports = {
   label: 'Pagination',
   context: {
-    list: fakerData
+    list: fakerData,
+    minimumNumber: minNum,
+    maximumNumber: maxNum
   }
 };
