a15de4ad031423a2bd0612aa19e0ceb0a0588d3b
JoelUCOP <joel.hagedorn@ucop.edu>
2019-02-08 10:20:15 -0800

    add slideshow 'read more' sample links

--- elements/3-components/slideshow.config.js
+++ elements/3-components/slideshow.config.js
@@ -1,4 +1,5 @@
 const faker = require('faker/locale/en');
+const samplelinks = ['https://calisphere.org', 'https://escholarship.org', 'https://dmptool.org', 'https://www.hathitrust.org'];
 const fakerData = [];
 
 for (var i = 0; i < 4; i++) {
@@ -6,6 +7,7 @@ for (var i = 0; i < 4; i++) {
     slide: {
       heading: faker.commerce.productName(),
       text: faker.lorem.sentences(2),
+      link: faker.helpers.randomize(samplelinks),
       imageSmall: 'https://loremflickr.com/740/500/nature?size=small&random=' + i,
       imageLarge: 'https://loremflickr.com/1095/740/nature?size=big&random=' + i,
       alt: faker.commerce.productName(),
