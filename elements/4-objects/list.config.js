d9c3192842435ef52f7279a1bd08d5a97eb5f8a4
JoelCDL <joel.hagedorn@ucop.edu>
2019-03-06 15:47:51 -0800

    create description list sample and styles

--- elements/4-objects/list.config.js
+++ elements/4-objects/list.config.js
@@ -5,6 +5,8 @@ for (var i = 0; i < 5; i++) {
   fakerList.push({
     list: {
       text: faker.lorem.sentence(),
+      term: faker.lorem.sentence(),
+      definition: faker.lorem.sentences()
     }
   });
 }
@@ -16,5 +18,5 @@ module.exports = {
   context: {
     list: fakerList
   },
-  notes: "By default, WP will not apply an inline style to `ul` or `ol` tags. Therefore, this setting is styled as a non-bulleted list. If 'circle', 'disc', or 'square' is selected for 'Bulleted list' in the editor, the inline style with that attribute will be added. 'Numbered list' in the WP editor does not have a non-numbered default list style. These styles are selected off of the WP editor 'entry-content' `div` wrapper."
+  notes: "By default, WP will not apply an inline style to `ul` or `ol` tags. Therefore, this setting is styled as a non-bulleted list. If 'circle', 'disc', or 'square' is selected for 'Bulleted list' in the editor, the inline style with that attribute will be added. 'Numbered list' in the WP editor does not have an alternative style."
 };
