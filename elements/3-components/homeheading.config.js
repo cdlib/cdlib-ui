8e00b3208df2e3c5f3763698b4953b9200258079
JoelUCOP <joel.hagedorn@ucop.edu>
2019-03-12 16:26:09 -0700

    create homeheading component; render within home page

---
+++ elements/3-components/homeheading.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  label: 'Home Heading',
+  context: {
+    heading: {
+      text: 'California Digital Library'
+    }
+  },
+  notes: 'This heading is visible for screen readers only (positioned off-screen).'
+};
