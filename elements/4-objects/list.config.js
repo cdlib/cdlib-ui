a05f0e3d761871b1e0a4932be360a25c07ea4bd7
JoelUCOP <joel.hagedorn@ucop.edu>
2019-03-26 16:28:58 -0700

    create compound list sample, unify list item indentation

--- elements/4-objects/list.config.js
+++ elements/4-objects/list.config.js
@@ -1,5 +1,8 @@
 const faker = require('faker/locale/en');
 const fakerList = [];
+const fakerCompoundList1 = [];
+const fakerCompoundList2 = [];
+const fakerCompoundList3 = [];
 
 for (var i = 0; i < 5; i++) {
   fakerList.push({
@@ -11,12 +14,41 @@ for (var i = 0; i < 5; i++) {
   });
 }
 
+for (var i = 0; i < 2; i++) {
+  fakerCompoundList1.push({
+    compoundlist1: {
+      text: faker.lorem.sentence(),
+      term: faker.lorem.sentence(),
+      definition: faker.lorem.sentences()
+    }
+  });
+}
+
+for (var i = 0; i < 2; i++) {
+  fakerCompoundList2.push({
+    compoundlist2: {
+      text: faker.lorem.sentence()
+    }
+  });
+}
+
+for (var i = 0; i < 2; i++) {
+  fakerCompoundList3.push({
+    compoundlist3: {
+      text: faker.lorem.sentence()
+    }
+  });
+}
+
 module.exports = {
   collated: true,
   default: '1',
   label: 'Lists',
   context: {
-    list: fakerList
+    list: fakerList,
+    compoundlist1: fakerCompoundList1,
+    compoundlist2: fakerCompoundList2,
+    compoundlist3: fakerCompoundList3,
   },
   notes: "By default, WP will not apply an inline style to `ul` or `ol` tags. Therefore, this setting is styled as a non-bulleted list. If 'circle', 'disc', or 'square' is selected for 'Bulleted list' in the editor, the inline style with that attribute will be added. 'Numbered list' in the WP editor does not have an alternative style."
 };
