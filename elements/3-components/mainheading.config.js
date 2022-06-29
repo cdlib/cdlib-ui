1ef481bcb9860749380f17f90bddb5fa4a59932b
Joel <jhagedorn@ucop.edu>
2022-06-29 16:12:28 -0700

    update main and half box components with sample data

--- elements/3-components/mainheading.config.js
+++ elements/3-components/mainheading.config.js
@@ -1,14 +1,17 @@
-const faker = require('faker/locale/en');
+const pages = require('../../sample-data/pages.json')
+const page = 37
+const pageHeading = pages[page].title.rendered
+const pageTagline = pages[page].acf.tagline
 
 module.exports = {
   label: 'Main Heading',
   context: {
     heading: {
       level: '1',
-      text: faker.commerce.productName()
+      text: pageHeading
     },
     tagline: {
-      text: faker.lorem.sentence()
+      text: pageTagline
     }
   }
 };
