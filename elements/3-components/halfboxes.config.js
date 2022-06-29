233083c8bf59fb8053154b24071a78da86cb0982
Joel <jhagedorn@ucop.edu>
2022-06-29 16:12:28 -0700

    update main and half box components with sample data

--- elements/3-components/halfboxes.config.js
+++ elements/3-components/halfboxes.config.js
@@ -1,4 +1,31 @@
+const pages = require('../../sample-data/pages.json')
+const page = 37
+const halfbox1Heading = pages[page].acf.side_by_side_box_1_heading
+const halfbox1Content = pages[page].acf.side_by_side_box_1_content
+const halfbox2Heading = pages[page].acf.side_by_side_box_2_heading
+const halfbox2Content = pages[page].acf.side_by_side_box_2_content
+
 module.exports = {
   label: 'Half Width Boxes',
-  notes: "If one of the two boxes is absent, the other one will stretch to fill the space."
-};
+  context: {
+    halfbox1: {
+      mainbox: {
+        heading: {
+          level: '2',
+          text: halfbox1Heading
+        },
+        text: halfbox1Content
+      }
+    },
+    halfbox2: {
+      mainbox: {
+        heading: {
+          level: '2',
+          text: halfbox2Heading
+        },
+        text: halfbox2Content
+      }
+    }
+  },
+  notes: "If one box is absent, the other one will stretch to fill the space."
+}
--- elements/3-components/mainbox.config.js
+++ elements/3-components/mainbox.config.js
@@ -1,33 +1,19 @@
-const faker = require('faker/locale/en');
-const paraData = [];
-
-for (var i = 0; i < 3; i++) {
-  if (process.env.NODE_ENV === 'testing') {
-    faker.seed(123);
-  }
-  paraData.push({
-    heading: {
-      level: '3'
-    },
-    link1: true,
-    link: {
-      text: faker.commerce.productName()
-    },
-    mainbox: {
-      text: faker.lorem.paragraph()
-    }
-  });
-}
+const pages = require('../../sample-data/pages.json')
+const page = 37
+const mainboxHeading = pages[page].acf.main_box_1_heading
+const mainboxContent = pages[page].acf.main_box_1_content
 
 module.exports = {
   collated: true,
   default: '1',
   label: 'Main Box',
   context: {
-    heading: {
-      level: '2',
-      text: 'Main Box Heading'
-    },
-    paragraph: paraData
+    mainbox: {
+      heading: {
+        level: '2',
+        text: mainboxHeading
+      },
+      text: mainboxContent
+    }
   }
-};
+}
