755717890c41c9184b1e91645af13673ff87c090
JoelCDL <joel.hagedorn@ucop.edu>
2022-06-30 16:17:54 -0700

    remove program page sidebars and legacy faker data

--- elements/1-pages/program.config.js
+++ elements/1-pages/program.config.js
@@ -1,25 +1,4 @@
-const faker = require('faker/locale/en');
-const fakeData = [];
-
-for (var i = 0; i < 2; i++) {
-  if (process.env.NODE_ENV === 'testing') {
-    faker.seed(123);
-  }
-  fakeData.push({
-    program: {
-      text: faker.lorem.paragraph()
-    }
-  });
-}
-
 module.exports = {
   label: 'Program',
-  preview: '@template-page',
-  context: {
-    paragraph: fakeData,
-    heading: {
-      level: '1',
-      text: 'Program Name'
-    }
-  }
+  preview: '@template-page'
 };
