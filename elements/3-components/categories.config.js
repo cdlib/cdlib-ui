b3a693f2ce7dab1aae0d65a67581ac4503f43ee2
JoelCDL <joel.hagedorn@ucop.edu>
2022-07-06 08:43:43 -0700

    apply sample data conversion for categories component

--- elements/3-components/categories.config.js
+++ elements/3-components/categories.config.js
@@ -1,46 +1,26 @@
-const faker = require('faker/locale/en');
-const fakerData1 = [];
-const fakerData2 = [];
-const fakerData3 = [];
+const categories = require('../../sample-data/categories.json')
+const categoryParents = categories.filter(el => el.parent === 0)
+const categoryChildren = categories.filter(el => el.parent !== 0)
+const categoriesFiltered = categoryParents.concat(categoryChildren)
+const categoriesMap = new Map()
+const categoryData = []
 
-for (var i = 0; i < 5; i++) {
-  if (process.env.NODE_ENV === 'testing') {
-    faker.seed(123);
-  }
-  fakerData1.push({
-    link: {
-      text: faker.commerce.productName()
-    }
-  });
-}
+// Nest JSON child objects within their parents using id and parent properties. Adapted from https://stackoverflow.com/questions/72320459/restructure-2-arrays-with-objects-as-a-nested-array:
 
-for (var i = 0; i < 3; i++) {
-  if (process.env.NODE_ENV === 'testing') {
-    faker.seed(123);
-  }
-  fakerData2.push({
-    link: {
-      text: faker.commerce.productName()
-    }
-  });
-}
+for (const category of categoriesFiltered) {
+  category.list = []
+  categoriesMap.set(category.id, category)
 
-for (var i = 0; i < 3; i++) {
-  if (process.env.NODE_ENV === 'testing') {
-    faker.seed(123);
+  if (category.parent === 0) {
+    categoryData.push(category)
+  } else {
+    categoriesMap.get(category.parent).list.push(category)
   }
-  fakerData3.push({
-    link: {
-      text: faker.commerce.productName()
-    }
-  });
 }
 
 module.exports = {
   label: 'Categories',
   context: {
-    list1: fakerData1,
-    list2: fakerData2,
-    list3: fakerData3
+    list: categoryData
   }
-};
+}
