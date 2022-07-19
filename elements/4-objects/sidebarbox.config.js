186b67445707be3cbabba380f709da12d4af3ddd
JoelCDL <joel.hagedorn@ucop.edu>
2022-07-18 17:04:09 -0700

    use sample data for sidebar box; include comp in page templates

--- elements/4-objects/sidebarbox.config.js
+++ elements/4-objects/sidebarbox.config.js
@@ -1,32 +1,19 @@
-const faker = require('faker/locale/en');
-const fakerData = [];
-
-function removePeriod(string) {
-  return string.replace('.', '');
-}
-
-for (var i = 0; i < 5; i++) {
-  fakerData.push({
-    link: {
-      text: removePeriod(faker.lorem.sentence()),
-      url: faker.internet.url(),
-      external: faker.random.boolean()
-    }
-  });
-}
+const pages = require('../../sample-data/pages.json')
+const page = 51
+const sidebarBoxHeading = pages[page].acf.sidebar_box_1_heading
+const sidebarBoxContent = pages[page].acf.sidebar_box_1_content
 
 module.exports = {
   collated: true,
   default: '1',
   label: 'Sidebar Box',
   context: {
-    heading: {
-      level: '2',
-      text: 'Sidebar Box Heading'
-    },
     sidebarbox: {
-      text: faker.lorem.paragraph()
-    },
-    listItem: fakerData
+      heading: {
+        level: '2',
+        text: sidebarBoxHeading
+      },
+      content: sidebarBoxContent
+    }
   }
-};
+}
