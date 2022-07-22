1cbb63f395b52bac9be740ff0ab2993f399cee98
JoelCDL <joel.hagedorn@ucop.edu>
2022-07-22 14:36:27 -0700

    use sample data for system status page and components

--- elements/3-components/status.config.js
+++ elements/3-components/status.config.js
@@ -1,22 +1,3 @@
-const faker = require('faker/locale/en');
-const tableData = [];
-
-for (var i = 0; i < 10; i++) {
-  if (process.env.NODE_ENV === 'testing') {
-    faker.seed(123);
-  }
-  tableData.push({
-    system: {
-      name: faker.company.companyName(),
-      status: faker.random.boolean(),
-      maintenance: faker.lorem.sentence()
-    }
-  });
-}
-
 module.exports = {
-  label: 'System Status',
-  context: {
-    row: tableData
-  }
-};
+  label: 'System Status'
+}
