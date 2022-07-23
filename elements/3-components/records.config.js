ccf39d3b515aeb5f4c449b111adad41e93679556
JoelCDL <joel.hagedorn@ucop.edu>
2022-07-22 17:00:00 -0700

    replace search records random data with sample data

--- elements/3-components/records.config.js
+++ elements/3-components/records.config.js
@@ -1,12 +1,3 @@
-const faker = require('faker/locale/en');
-const query = faker.commerce.product()
-
 module.exports = {
-  label: 'Search Records',
-  context: {
-    records: {
-      total: faker.random.number(2000),
-      query: query.toLowerCase()
-    }
-  }
-};
+  label: 'Search Records'
+}
