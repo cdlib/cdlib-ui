de121a39f75714d37a79c1ee3005dbf021a0355e
JoelCDL <joel.hagedorn@ucop.edu>
2024-07-17 10:26:52 -0700

    Replace empty href link attributes in sample pages with cdlib url

--- elements/3-components/breadcrumb.config.js
+++ elements/3-components/breadcrumb.config.js
@@ -4,6 +4,7 @@ const breadcrumbLevels = ['California Digital Library', 'Level 2', 'Level 3']
 for (const breadcrumbLevel of breadcrumbLevels) {
   breadcrumbSamples.push({
     link: {
+      url: 'https://cdlib.org',
       text: breadcrumbLevel
     }
   });
