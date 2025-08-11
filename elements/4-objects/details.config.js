9b2cdb5cf8084808b7fd7313bafbdbbfdbdb7bd8
JoelCDL <jhagedorn@ucop.edu>
2025-08-11 16:07:45 -0700

    Add sample text before and after details object

--- elements/4-objects/details.config.js
+++ elements/4-objects/details.config.js
@@ -5,7 +5,9 @@ module.exports = {
   context: {
     details: {
       summary: faker.lorem.sentence(),
-      text: faker.lorem.sentences(20)
+      text: faker.lorem.sentences(20),
+      text_before: faker.lorem.paragraphs(3),
+      text_after: faker.lorem.paragraphs(3)
     }
   },
   notes: 'A child element, like `<p>`, must wrap the content within the parent `<details>` element in order for details-element-polyfill.js to work in IE.'
