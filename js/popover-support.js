87b92395b2d5f19196735d08dd36bc2f0b4962dd
JoelCDL <jhagedorn@ucop.edu>
2025-12-17 15:28:43 -0800

    Update popover support with exportable function

--- js/popover-support.js
+++ js/popover-support.js
@@ -2,6 +2,12 @@
 
 // If the popover element is detected in the browser, remove the no-popover class from the html tag.
 
-if (window.HTMLElement.prototype.hasOwnProperty('popover')) { // eslint-disable-line no-prototype-builtins
+const popoverSupport = () => {
+  return Object.hasOwn(window.HTMLElement.prototype, 'popover')
+}
+
+if (popoverSupport()) {
   document.querySelector('html').classList.remove('no-popover')
 }
+
+export { popoverSupport }
