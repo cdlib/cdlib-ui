291f1feddc040543b6f2383cf48ffa1279dc3887
JoelCDL <jhagedorn@ucop.edu>
2025-12-17 15:29:44 -0800

    Reduce anchor positioning support to anchor scope

--- src/js/anchor-positioning.js
+++ src/js/anchor-positioning.js
@@ -7,13 +7,9 @@
 const anchorPositioningPolyfill = async () => {
   const { default: polyfill } = await import('@oddbird/css-anchor-positioning/dist/css-anchor-positioning-fn.js')
 
-  polyfill({
-    elements: undefined,
-    excludeInlineStyles: false,
-    useAnimationFrame: false
-  })
+  polyfill()
 }
 
-if (!('anchorName' in document.documentElement.style)) {
+if (!('anchorScope' in document.documentElement.style)) {
   anchorPositioningPolyfill()
 }
