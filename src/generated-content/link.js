aea658c22e1ece1503d5f9409bd70fae6776fc5f
JoelCDL <jhagedorn@ucop.edu>
2026-02-13 16:30:10 -0800

    Add links object to toolkit

--- src/generated-content/link.js
+++ src/generated-content/link.js
@@ -1,51 +1,72 @@
 module.exports = {
   link: {
     text: 'Default link to cdlib.org page',
-    url: 'https://cdlib.org',
-    linebreak: true
+    url: 'https://cdlib.org'
   },
-  link: {
-    text: 'Anchor link to reference',
-    url: '#some-heading'
+  sample2: {
+    link: {
+      text: 'Anchor link to reference',
+      url: '#some-heading'
+    }
   },
-  link: {
-    text: 'Link to external site',
-    url: 'wordpress/external/link'
+  sample3: {
+    link: {
+      text: 'Link to external site',
+      url: 'wordpress/external/link'
+    }
   },
-  link: {
-    text: 'Link to email address',
-    url: 'mailto:someone@cdlib.org'
+  sample4: {
+    link: {
+      text: 'Link to email address',
+      url: 'mailto:someone@cdlib.org'
+    }
   },
-  link: {
-    text: 'Link to UCOP Sharepoint page',
-    url: 'https://cdlib.org/contact/cdl-document-request?document-link=https://ucofficeofthepresident.sharepoint.com/sites/cdl/cpa/licensed-resources'
+  sample5: {
+    link: {
+      text: 'Link to UCOP Sharepoint page',
+      url: 'https://cdlib.org/contact/cdl-document-request?document-link=https://ucofficeofthepresident.sharepoint.com/sites/cdl/cpa/licensed-resources'
+    }
   },
-  link: {
-    text: 'Link to PDF',
-    url: 'wordpress/document.pdf'
+  sample6: {
+    link: {
+      text: 'Link to PDF file',
+      url: 'sample-url/document.pdf'
+    }
   },
-  link: {
-    text: 'Link to Excel spreadsheet on UCOP Sharepoint',
-    url: 'https://cdlib.org/contact/cdl-document-request?document-link=https://ucofficeofthepresident.sharepoint.com/sites/cdl/cpa/licensed-resources/CDL-licensed-resources-with-tier-info.xlsx'
+  sample7: {
+    link: {
+      text: 'Link to Excel spreadsheet on UCOP Sharepoint',
+      url: 'https://cdlib.org/contact/cdl-document-request?document-link=https://ucofficeofthepresident.sharepoint.com/sites/cdl/cpa/licensed-resources/CDL-licensed-resources-with-tier-info.xlsx'
+    }
   },
-  link: {
-    text: 'Link to Word document',
-    url: 'wordpress/document.doc'
+  sample8: {
+    link: {
+      text: 'Link to Word file',
+      url: 'sample-url/document.doc'
+    }
   },
-  link: {
-    text: 'Link to Excel spreadsheet',
-    url: 'wordpress/document.xls'
+  sample9: {
+    link: {
+      text: 'Link to Excel spreadsheet',
+      url: 'sample-url/document.xls'
+    }
   },
-  link: {
-    text: 'Link to text file',
-    url: 'wordpress/document.txt'
+  sample10: {
+    link: {
+      text: 'Link to text file',
+      url: 'sample-url/document.txt'
+    }
   },
-  link: {
-    text: 'Link to ZIP file',
-    url: 'wordpress/document.zip'
+  sample11: {
+    link: {
+      text: 'Link to Zip file',
+      url: 'sample-url/document.zip'
+    }
   },
-  link: {
-    text: 'Link to Rich Text file',
-    url: 'wordpress/document.rtf'
+  sample12: {
+    link: {
+      text: 'Link to Rich Text file',
+      url: 'sample-url/document.rtf'
+    }
   }
 }
