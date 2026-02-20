c79c4a178b10b4033f2532512ea89501b042541e
JoelCDL <jhagedorn@ucop.edu>
2026-02-19 17:11:43 -0800

    Add headings object to toolkit

--- src/generated-content/heading.js
+++ src/generated-content/heading.js
@@ -1,27 +1,52 @@
+const { faker } = require('@faker-js/faker/locale/en');
+const headingText = titleCase(faker.lorem.words(10))
+
+function titleCase(str) {
+  return str.toLowerCase().split(' ').map(function(word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
+}
 
 module.exports = {
   heading: {
     level: '1',
-    text: 'Astronomy, Indigenous Knowledge and Interpretation: Advancing studies of Cultural Astronomy in South Africa',
-    abbr: true
+    text: headingText
   },
-  // Linked headings using Link objects:
-  link1: false,
-  link2: false,
-  link: {
-    text: 'Linked heading text'
+  heading_sample2: {
+    heading: {
+      level: '2',
+      text: headingText
+    }
   },
-  heading: {
-    level: '2'
+  heading_sample3: {
+    heading: {
+      level: '2',
+      link: {
+        text: headingText,
+        url: 'https://cdlib.org'
+      }
+    }
   },
-  heading: {
-    level: '3'
+  heading_sample4: {
+    heading: {
+      level: '3',
+      text: headingText,
+      abbr: {
+        title: 'California Digital Library',
+        text: 'CDL'
+      }
+    }
   },
-  heading: {
-    level: '4'
+  heading_sample5: {
+    heading: {
+      level: '4',
+      text: headingText
+    }
   },
-  heading: {
-    level: '2',
-    text: 'FAQ Heading: Astronomy, Indigenous Knowledge and Interpretation'
+  heading_sample6: {
+    heading: {
+      level: '5',
+      text: headingText
+    }
   }
 }
