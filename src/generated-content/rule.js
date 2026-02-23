78afb1f00745ee85abb0b932ae0e3f1704217c39
JoelCDL <jhagedorn@ucop.edu>
2026-02-23 11:30:18 -0800

    Add rules object to toolkit

--- src/generated-content/rule.js
+++ src/generated-content/rule.js
@@ -2,7 +2,15 @@ const { faker } = require('@faker-js/faker/locale/en');
 
 module.exports = {
   rule: {
-    before: faker.lorem.sentences(10),
-    after: faker.lorem.sentences(10)
+    text_before: {
+      paragraph: {
+        text: faker.lorem.sentences(10)
+      }
+    },
+    text_after: {
+      paragraph: {
+        text: faker.lorem.sentences(10)
+      }
+    }
   }
 }
