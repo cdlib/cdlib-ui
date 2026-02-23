5fe1cc4ba6f10a73266314e6dfb6e79768f7aa38
JoelCDL <jhagedorn@ucop.edu>
2026-02-23 15:38:46 -0800

    Add contact component to toolkit

--- src/generated-content/contact.js
+++ src/generated-content/contact.js
@@ -6,7 +6,8 @@ module.exports = {
       text: 'UCOP Contact Information'
     },
     link: {
-      text: 'UCSD Contact Information'
+      text: 'UCSD Contact Information',
+      url: 'https://act.ucsd.edu/directory/faculty_staff?jlinkevent=Default&last=Woo&first=Michelle'
     }
   }
 }
