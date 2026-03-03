50e090aebb50d5febf31fcde00123b2088a56598
JoelCDL <jhagedorn@ucop.edu>
2026-03-03 11:54:03 -0800

    Add wells component to toolkit

--- src/generated-content/well.js
+++ src/generated-content/well.js
@@ -2,20 +2,38 @@ const { faker } = require('@faker-js/faker/locale/en');
 
 module.exports = {
   well: {
-    heading: {
-      level: 2,
-      text: faker.commerce.productName()
+    heading_1: {
+      heading: {
+        level: 2,
+        text: faker.commerce.productName()
+      }
     },
-    well: {
-      text: faker.lorem.sentences(10)
+    paragraph: {
+      text: 'Paragraph text. ' + faker.lorem.sentences(10)
     },
-    link: {
-      text: faker.commerce.productName()
+    blockquote: {
+      text: 'Blockquote text. ' + faker.lorem.sentences(10)
     },
     figure: {
       alignmentClass: 'alignleft',
       imageUrl: 'http://loremflickr.com/300/200/nature',
       caption: faker.lorem.sentences(2)
+    },
+    heading_2: {
+      heading: {
+        level: 2,
+        text: 'This Heading Shouldn\'t Float Right of Figure Above'
+      }
+    },
+    link_1: {
+      link: {
+        text: faker.commerce.productName()
+      }
+    },
+    link_2: {
+      link: {
+        text: faker.commerce.productName()
+      }
     }
   }
 }
